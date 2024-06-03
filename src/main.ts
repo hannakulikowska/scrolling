f705cb4a73b5b61ba46567a1f3e608c78690a6b3
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-06-03 10:05:51 +0200

    feat: add aos animation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import './style.scss';
+import 'aos/dist/aos.css';
+import Aos from 'aos';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('typescript is rinnung');
+  Aos.init({
+    once: false,
+  });
+});
